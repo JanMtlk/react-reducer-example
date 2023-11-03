a6c05dd45474bbf84b34312eaf6ab63580432c14
Jan Mátl <75473807+JanMtlk@users.noreply.github.com>
2023-11-03 09:55:11 +0100

    initialize with use reducer example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
 
+
+
+type TodoType = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+function todoReducer (state: TodoType[], action: {
+  type: 'ADD_TODO';
+  text: string;
+} | {
+  type: 'TOGGLE_TODO';
+  id: number;
+} | {
+  type: 'REMOVE_TODO';
+  id: number;
+}) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat({
+        id: Math.random(), // not really unique
+        text: action.text,
+        done: false
+      });
+    case 'TOGGLE_TODO':
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error();
+  }
+
+}
 function App() {
+  const [todos, todosDispatch] = useReducer(todoReducer, []);
+  const [text, setText] = React.useState('');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map(todo => (
+        <div key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.done}
+            onChange={() => todosDispatch({ type: 'TOGGLE_TODO', id: todo.id })}
+          />
+          {todo.text}
+          <button onClick={() => todosDispatch({ type: 'REMOVE_TODO', id: todo.id })}>Remove</button>
+          </div>
+      ))}
+
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          todosDispatch({ type: 'ADD_TODO', text });
+          setText('');
+        }}
+      >
+        <input value={text} onChange={e => setText(e.target.value)} />
+        <button type="submit">Add Todo</button>
+        </form>
     </div>
   );
 }
